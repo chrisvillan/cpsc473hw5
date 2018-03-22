7cf1a298e0652fef97593913831aae8f023b2b79
chrisvillan <villanuevachris19@gmail.com>
2018-03-21 23:51:42 -0700

    added custom page for 404 error. added /topic for websockets. beautified.

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
+var errorPath = ('app/error.html')
+var mime = require("mime");
 
-var handleError = function (err, res) {
-  res.writeHead(404);
-  res.end();
+var handleError = function(err, res) {
+  fs.readFile(errorPath, function(err, data) {
+    res.writeHead(404, {
+      'Content-Type': mime.getType(errorPath)
+    });
+    res.write(data);
+    res.end();
+  });
 };
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
   console.log('Responding to a request.');
   var filePath = extract(req.url);
-  fs.readFile(filePath, function (err, data) {
+  fs.readFile(filePath, function(err, data) {
     if (err) {
       handleError(err, res);
       return;
     } else {
-      res.setHeader('Content-Type','text/html');
+      res.setHeader('Content-Type', 'text/html');
       res.end(data);
     }
     res.end(data);
--- websockets-server.js
+++ websockets-server.js
@@ -5,21 +5,35 @@ var ws = new WebSocketServer({
   port: port
 });
 var messages = [];
+var topic;
 
 console.log('websockets server started');
 
-ws.on('connection', function (socket) {
+ws.on('connection', function(socket) {
   console.log('client connection established');
 
-  messages.forEach(function (msg) {
+
+  if (topic) {
+    socket.send("*** Topic is '" + topic + "'");
+  }
+
+  messages.forEach(function(msg) {
     socket.send(msg);
   });
 
-  socket.on('message', function (data) {
+  socket.on('message', function(data) {
     console.log('message received: ' + data);
-    messages.push(data);
-    ws.clients.forEach(function (clientSocket) {
-      clientSocket.send(data)
-    });
+    if (data.indexOf("/topic") != -1) {
+      var changedTopic = "*** Topic has changed to " + "'" + data.substring(7, data.length) + "'";
+      topic = data.substring(7, data.length);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(changedTopic);
+      });
+    } else {
+      messages.push(data);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(data);
+      });
+    }
   });
 });
